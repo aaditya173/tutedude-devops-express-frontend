8fa4643bb47ae2ea12b0578e1db1ac6c2e7cd5b9
Aditya Kasaudhan <adityakld13@gmail.com>
2026-04-02 11:37:04 -0400

    add express frontend files

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+// Access values from .env
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost";
+const BACKEND_PORT = process.env.BACKEND_PORT || 5000;
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("form");
+});
+
+app.post("/submit", async (req, res) => {
+    try {
+        const response = await axios.post(`${BACKEND_URL}:${BACKEND_PORT}/process`, {
+            name: req.body.name,
+            email: req.body.email
+        });
+
+        res.send(response.data);
+    } catch (error) {
+        res.send("Error connecting to backend");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Frontend running on port 3000");
+});
